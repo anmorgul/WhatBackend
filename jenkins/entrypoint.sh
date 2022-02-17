088ce1e9bc45f9f707350d88453fc20eee6e1a0f
anmorgul <anmorgul@gmail.com>
2022-02-17 02:35:37 +0200

    te23

--- jenkins/entrypoint.sh
+++ jenkins/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 sed -i -r "s|(\"Http\":.*)|\"Http\": \"$APIURL\"|" /app/appsettings.DevelopmentLocalhost.json
-sed -i -r "s|(\"Http\":.*)|\"Http\": \"$APIURL\",|" /app/appsettings.json
-sed -i -r "s|(\"Https\":.*)|\"Https\": \"$APIURL\"|" /app/appsettings.json
+# sed -i -r "s|(\"Http\":.*)|\"Http\": \"$APIURL\",|" /app/appsettings.json
+# sed -i -r "s|(\"Https\":.*)|\"Https\": \"$APIURL\"|" /app/appsettings.json
+
+sed -i -r "s|(\"Http\":.*)|\"Http\": \"$APIURL\"|" /app/appsettings.json
+sed -i -r "s|(\"Https\":.*)||" /app/appsettings.json
 
 dotnet CharlieBackend.Panel.dll
